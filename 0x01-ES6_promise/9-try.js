ce87f2292051709572f9320c020638d6847d6d7f
AIMAD KACEM <kacem.aimad@gmail.com>
2024-07-27 18:01:08 +0100

    9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,8 +4,8 @@ export default function guardrail(mathFunction) {
     const res = mathFunction();
     queue.push(res);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(String(error));
   }
-  queue.push('Guardrail was processed')
+  queue.push('Guardrail was processed');
   return queue;
 }
