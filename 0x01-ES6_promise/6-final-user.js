8b3c986bcf26529f290e54913b6f9014d0867c4b
AIMAD KACEM <kacem.aimad@gmail.com>
2024-07-27 17:36:12 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(fileName, lastName);
+  const signUp = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
-  const results = await Promise.allSettled([signUp, photo]);
-
-  return results.map(result => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason
-  }));
+  return Promise
+    .allSettled([signUp, photo])
+    .then((results) => (
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }))
+    ));
 }
