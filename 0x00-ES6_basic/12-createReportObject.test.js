6b259f6bc980fcbe2b26885b041bc329fc4c8ad3
AIMAD KACEM <kacem.aimad@gmail.com>
2024-07-27 16:42:36 +0100

    11-createEmployeesObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.test.js
@@ -0,0 +1,26 @@
+import createEmployeesObject from './11-createEmployeesObject.js';
+import createReportObject from './12-createReportObject.js';
+
+const engineering = ['John Doe', 'Guillaume Salva'];
+const marketing = ['Agatha Christie', 'Jason Leverson'];
+const design = ['Kanye East', 'Jay Li'];
+
+test('createReportObject returns the correct object', () => {
+    const employees = {
+      ...createEmployeesObject('engineering', engineering),
+      ...createEmployeesObject('marketing', marketing),
+      ...createEmployeesObject('design', design),
+    };
+  
+    const holbertonEmployees = {
+      engineering,
+      marketing,
+      design,
+    };
+  
+    const report = createReportObject(employees);
+  
+    expect(report.allEmployees).toEqual(holbertonEmployees);
+    expect(report.getNumberOfDepartments(report.allEmployees)).toEqual(3);
+  });
+  
