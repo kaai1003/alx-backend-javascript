e148e2383ac00be6f9e6841ba88b351d83dceb1f
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-18 22:05:59 +0100

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('node:fs');
+
+const countStudents = (path) => {
+  if (!fs.existsSync(path) || !fs.statSync(path).isFile) {
+    throw new Error('Cannot load the database');
+  }
+  const dt = fs.readFileSync(path, 'utf-8');
+  const lines = dt.split(/\r?\n/)
+    .filter(line => line.trim() !== '');
+  const students = lines.slice(1);
+  console.log(`Number of students: ${students.length}`);
+  const studentFields = {};
+  for (const student of students) {
+    const details = student.split(',');
+    const firstName = details[0];
+    const field = details[3];
+
+    if (!studentFields[field]) {
+      studentFields[field] = [];
+    }
+    studentFields[field].push(firstName);
+  }
+  for (const [f, sl] of Object.entries(studentFields)) {
+    console.log(`Number of students in ${f}: ${sl.length}. List: ${sl.join(', ')}`);
+  }
+};
+
+module.exports = countStudents;
