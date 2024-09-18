7d6c2b45f5a81b4554cd1c211a0143c344287884
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-18 23:19:57 +0100

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,16 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+const app = http.createServer();
+app.on('request', (_, response) => {
+  const outPut = 'Hello Holberton School!';
+  response.setHeader('Content-Type', 'text/plain');
+  response.setHeader('Content-Length', outPut.length);
+  response.statusCode = 200;
+  response.write(Buffer.from(outPut));
+});
 
-  app.listen(1234, () => {
-    console.log('Server is listening on port 1234');
-  });
+app.listen(1234, 'localhost', () => {
+  console.log('Server is listening on port 1234');
 });
 
 module.exports = app;
