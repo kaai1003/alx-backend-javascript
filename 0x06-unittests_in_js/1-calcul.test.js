5add9a0203d1f8510536af2a19da136d6efe3009
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-20 13:42:21 +0100

    1-calcul.js

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function() {
+  it('should return 5 for SUM of 1 - 3.7', function() {
+    assert.equal(calculateNumber('SUM', 1, 3.7), 5);
+  });
+
+  it('should return 6 for SUM of 1.5 + 3.7', function() {
+    assert.equal(calculateNumber('SUM', 1.5, 3.7), 6);
+  });
+
+  it('should return 0 for SUBTRACT of 2.57099 - 3.1054', function() {
+    assert.equal(calculateNumber('SUBTRACT', 2.57099, 3.1054), 0);
+  });
+
+  it('should return 7 for SUBTRACT of 3.6054 - 2.57099', function() {
+    assert.equal(calculateNumber('SUBTRACT', 2.57099, 3.6054), 1);
+  });
+
+  it('should return 7 for SUBTRACT of 0.57099 - 3.2054', function() {
+    assert.equal(calculateNumber('SUBTRACT', 3.2054 , 0.57099), -2);
+  });
+
+  it('should return 0.5 for DIVIDE of 2.47099 / 3.6054', function() {
+    assert.equal(calculateNumber('DIVIDE', 2.47099, 3.6054), 0.5);
+  });
+
+  it('should return Error for DIVIDE of 2.57099 / 0.232', function() {
+    assert.equal(calculateNumber('DIVIDE', 2.57099, 0.232), 'Error');
+  });
+
+  it('should return 3 for DIVIDE of 2.57099 / 0.532', function() {
+    assert.equal(calculateNumber('DIVIDE', 2.57099, 0.532), 3);
+  });
+});
