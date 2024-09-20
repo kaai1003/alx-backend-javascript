0a109618281094e8addcf55e1cfe8fd9854134de
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-20 13:57:14 +0100

    1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -2,8 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', function() {
-  it('should return 5 for SUM of 1 - 3.7', function() {
+  it('should return 5 for SUM of 1 + 3.7', function() {
     assert.equal(calculateNumber('SUM', 1, 3.7), 5);
+  });
+
+  it('should return 4 for SUM of -1 + 3.7', function() {
+    assert.equal(calculateNumber('SUM', -1, 3.7), 3);
+  });
+
+  it('should return -5 for SUM of -1 + (-3.7)', function() {
+    assert.equal(calculateNumber('SUM', -1, -3.7), -5);
   });
 
   it('should return 6 for SUM of 1.5 + 3.7', function() {
@@ -14,8 +22,16 @@ describe('calculateNumber', function() {
     assert.equal(calculateNumber('SUBTRACT', 2.57099, 3.1054), 0);
   });
 
-  it('should return 7 for SUBTRACT of 3.6054 - 2.57099', function() {
+  it('should return 1 for SUBTRACT of 3.6054 - 2.57099', function() {
     assert.equal(calculateNumber('SUBTRACT', 2.57099, 3.6054), 1);
+  });
+
+  it('should return -1 for SUBTRACT of (-3.6054) - (-2.57099)', function() {
+    assert.equal(calculateNumber('SUBTRACT', -2.57099, -3.6054), -1);
+  });
+
+  it('should return 7 for SUBTRACT of (-3.6054) - 2.57099', function() {
+    assert.equal(calculateNumber('SUBTRACT', 2.57099, -3.6054), -7);
   });
 
   it('should return 7 for SUBTRACT of 0.57099 - 3.2054', function() {
@@ -32,5 +48,13 @@ describe('calculateNumber', function() {
 
   it('should return 3 for DIVIDE of 2.57099 / 0.532', function() {
     assert.equal(calculateNumber('DIVIDE', 2.57099, 0.532), 3);
+  });
+
+  it('should return 0 for DIVIDE of 0.17099 / 3', function() {
+    assert.equal(calculateNumber('DIVIDE', 0.17099, 3), 0);
+  });
+
+  it('should return Error for DIVIDE of 0 / 0.132', function() {
+    assert.equal(calculateNumber('DIVIDE', 0, 0.132), 'Error');
   });
 });
