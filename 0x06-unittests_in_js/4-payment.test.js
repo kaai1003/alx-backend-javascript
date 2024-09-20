67834108533e573e47fc13aa6007c1b2cbfa392d
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-21 00:35:04 +0100

    4-payment.test.js

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,17 @@
+const sinn = require('sinon');
+const assert = require('assert');
+const {describe, it, utils} = require('mocha');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  it('validate the usage of the Utils function', function() {
+    const spy = sinn.spy(console, "log");
+    const stub = sinn.stub(Utils, "calculateNumber").returns(10);
+    sendPaymentRequestToApi(100, 20);
+    assert(spy.withArgs("The total is: 10").calledOnce);
+    assert(stub.withArgs('SUM', 100, 20).calledOnce);
+    spy.restore();
+    stub.restore();
+  });
+});
