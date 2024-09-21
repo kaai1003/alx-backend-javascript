9ee25f85fb1325ec22eb6d55f0e49bee161c0ddd
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-21 15:50:21 +0100

    run api.js separatly

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,13 @@
 const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
-const app = require('./api');
 let appTest;
 
 chai.use(chaiHttp);
 
 describe('test suite for the index page', function() {
     it('should return 200 status', (done) => {
-        chai.request(app)
+        chai.request('http://localhost:7865')
             .get('/')
             .end((err, res) => {
                 expect(res).to.have.status(200);
@@ -17,7 +16,7 @@ describe('test suite for the index page', function() {
     });
 
     it('should return message "Welcome to the payment system"', (done) => {
-        chai.request(app)
+        chai.request('http://localhost:7865')
             .get('/')
             .end((err, res) => {
                 expect(res.text).to.equal('Welcome to the payment system');
