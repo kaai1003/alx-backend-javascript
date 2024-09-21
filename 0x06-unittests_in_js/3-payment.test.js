29709ac1270f6e6afbc55fb63eec0519dc08ec74
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-21 11:35:49 +0100

    3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,12 +1,11 @@
 const sinn = require('sinon');
 const assert = require('assert');
-const {describe, it, utils} = require('mocha');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', function() {
   it('validate the usage of the Utils function', function() {
-    const spy = sinn.spy(Utils, "calculateNumber");
+    const spy = sinon.spy(Utils, "calculateNumber");
     sendPaymentRequestToApi(3.245, 5.6);
     assert(spy.calledOnce);
     spy.restore();
