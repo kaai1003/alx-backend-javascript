139c699b6bcfcdf2f62c02fd4449f4065920b952
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-22 00:42:38 +0100

    3-read_file_async.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,22 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
+const port = 1245;
 
-const app = http.createServer();
+const app = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.end('Hello Holberton School!');
+    } else if (req.url === '/students') {
+        const path = process.argv[2];
+        res.statusCode = 200;
+        res.end(`This is the list of our students\n${countStudents(path)}`);
+    }
+});
+
+app.listen(port, () => {
+    console.log()
+})
 
 module.exports = app;
